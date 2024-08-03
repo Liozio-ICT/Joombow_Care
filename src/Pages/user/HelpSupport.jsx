de1901d2c087c49e5bff81a7719cc74915e9c73c
Siyaka Fatimat <siyakafatimat@gmail.com>
2024-08-03 11:39:11 +0100

    Social media links added

--- src/Pages/user/HelpSupport.jsx
+++ src/Pages/user/HelpSupport.jsx
@@ -1,21 +1,31 @@
 import ProfileListItem from "../../components/ProfileListItem";
-import { FaInstagram, FaRegEnvelope, FaXTwitter } from "react-icons/fa6";
+import { FaFacebook, FaInstagram, FaRegEnvelope, FaWhatsapp, FaXTwitter } from "react-icons/fa6";
 import { ScrollRestoration } from "react-router-dom";
 
 const list = [
   {
     label: "Reach out on X(Twitter)",
-    to: "x.com",
+    to: "https://x.com/Liozio_Joombow/status/1817858442683396459?t=dvdV-ldWHW0HRavST3wKyw&s=19",
     icon: <FaXTwitter />,
   },
   {
     label: "Reach out on Instagram",
-    to: "instagram.com",
+    to: "https://x.com/Liozio_Joombow/status/1817858442683396459?t=dvdV-ldWHW0HRavST3wKyw&s=19",
     icon: <FaInstagram />,
   },
   {
+    label: "Reach out on Facebook",
+    to: "https://www.facebook.com/share/pNNwjpkqrtggdzvW/?mibextid=qi2Omg",
+    icon: <FaFacebook />,
+  },
+  {
+    label: "Reach out on WhatsApp",
+    to: "https://wa.me/2348122969820",
+    icon: <FaWhatsapp />,
+  },
+  {
     label: "Reach out on E-mail",
-    to: "mailto:",
+    to: "mailto:fatimat@liozio.com",
     icon: <FaRegEnvelope />,
   },
 ];
