94695656fd5e4c0ab249fb2aa656c12ea91dcd69
ccanokwuru <cccanoks@gmail.com>
2024-10-01 14:32:55 +0100

    revert to remote server

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,8 +1,8 @@
 import ky from "ky";
 
-// export const API_URL = "https://car-care-backend-joombow.onrender.com/";
+export const API_URL = "https://car-care-backend-joombow.onrender.com/";
 // export const API_URL = "https://car-care-backend-joombow.vercel.app/";
-export const API_URL = "http://127.0.0.1:3000/";
+// export const API_URL = "http://127.0.0.1:3000/";
 
 // Create a function to handle API requests using fetch
 const apiClient = ky.create({
